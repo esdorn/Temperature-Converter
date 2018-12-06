7248f2011d95c64b39a4969ba4ae4adb96055ab9
esdorn <esdorn@stu.naperville203.org>
2018-12-06 11:41:09 -0600

    added the swap method

--- app/src/main/java/com/nchsprograming/temperatureconverter/MainActivity.java
+++ app/src/main/java/com/nchsprograming/temperatureconverter/MainActivity.java
@@ -3,8 +3,11 @@ package com.nchsprograming.temperatureconverter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private boolean swap = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,8 +15,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
 
-    public void calculateTemperature(View view) {
-        
-
+    public void Swap(View view)
+    {
+        TextView textView = findViewById(R.id.textView2);
+        if (swap)
+        {
+            textView.setText("Celsius > Fahrenheit");
+            swap = false;
+        }
+        else
+        {
+            textView.setText("Fahrenheit > Celsius");
+            swap = true;
+        }
     }
 }
